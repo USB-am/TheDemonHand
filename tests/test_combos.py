cde8c6adb2c1a51ec20e46b2703ebf85c09bc349
USB-am <smit.acer@mail.ru>
2026-08-06 23:27:04 +0300

    Created tests for all Combo.get_combo_cards methods

--- tests/test_combos.py
+++ tests/test_combos.py
@@ -32,9 +32,28 @@ COMBO_TEST_CASES = [
     ([1,2,3,4,6], ['sun','fire','moon','stone','sun'], SoloCombo),
 ]
 
-
 @pytest.mark.parametrize("values, suits, expected", COMBO_TEST_CASES)
 def test_combos(values, suits, expected):
     cards = make_cards(values, [Suit[s.upper()] for s in suits])
     result = get_combo(cards)
+    assert result == expected
+
+
+COMBO_CARDS_TEST_CASES = [
+    (make_cards([10]*5, [Suit.SUN]*5), DemonHandCombo, make_cards([10]*5, [Suit.SUN]*5)),
+    (make_cards([2, 3, 4, 5, 6], [Suit.SUN]*5), MarchingHordeCombo, make_cards([2, 3, 4, 5, 6], [Suit.SUN]*5)),
+    (make_cards([5, 5, 5, 5, 7], [Suit.SUN]*5), TetradCombo, make_cards([5, 5, 5, 5], [Suit.SUN]*4)),
+    (make_cards([2, 5, 5, 5, 5], [Suit.SUN]*5), TetradCombo, make_cards([5, 5, 5, 5], [Suit.SUN]*4)),
+    (make_cards([3, 3, 3, 7, 7], [Suit.SUN]*5), GrandWarhostCombo, make_cards([3, 3, 3, 7, 7], [Suit.SUN]*5)),
+    (make_cards([2, 4, 6, 8, 7], [Suit.SUN]*5), HordeCombo, make_cards([2, 4, 6, 8, 7], [Suit.SUN]*5)),
+    (make_cards([2, 3, 4, 5, 6], [Suit.SUN, Suit.FIRE, Suit.MOON, Suit.SUN, Suit.STONE]), MarchCombo, make_cards([2, 3, 4, 5, 6], [Suit.SUN, Suit.FIRE, Suit.MOON, Suit.SUN, Suit.STONE])),
+    (make_cards([5, 7, 5, 5, 6], [Suit.SUN]*5), TriadCombo, make_cards([5, 5, 5], [Suit.SUN]*3)),
+    (make_cards([2, 4, 5, 2, 5], [Suit.SUN]*5), DyadSetCombo, make_cards([2, 2, 5, 5], [Suit.SUN]*4)),
+    (make_cards([2, 3, 7, 8, 2], [Suit.SUN]*5), DyadCombo, make_cards([2, 2], [Suit.SUN]*2)),
+    (make_cards([2, 9, 5, 4, 7], [Suit.SUN]*5), SoloCombo, make_cards([9], [Suit.SUN])[0]),
+]
+
+@pytest.mark.parametrize("cards, combo, expected", COMBO_CARDS_TEST_CASES)
+def test_get_combo_cards(cards, combo, expected):
+    result = combo.get_combo_cards(cards)
     assert result == expected
